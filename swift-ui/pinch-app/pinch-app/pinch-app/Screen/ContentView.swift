3e9e377386e228c86ead477d939b5a9c2ccfcc0d
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-10-16 00:40:25 +0000

    .

--- swift-ui/pinch-app/pinch-app/pinch-app/Screen/ContentView.swift
+++ swift-ui/pinch-app/pinch-app/pinch-app/Screen/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
 //    MARK: Property
     
     @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero // CGSize(width: 0, height: 0)
     
 //    MARK: Function
     
@@ -28,6 +30,22 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                // MARK: 1. Tap Gesture
+                    .onTapGesture (count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+                // MARK: 2. Drag Gesture
+                
             } //: ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
